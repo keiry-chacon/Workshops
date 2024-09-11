d0b2ff6772411724b416f3dc6e1cb1df1d78977b
keiry-chacon <keirychas@gmail.com>
2024-09-11 09:37:15 -0600

    Add files via upload

---
+++ Workshop1/workshop1.php
@@ -0,0 +1,8 @@
+<?php
+  date_default_timezone_set('America/Costa_Rica');
+
+  $fechaActual = date("d-m-Y");
+  $horaActual = date("h:i:s");
+
+  echo "La fecha es: $fechaActual y la hora es $horaActual " ;
+?>
