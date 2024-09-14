2c86ae400bf42bf64e1c9eaeb1b0099423337806
keiry-chacon <keirychas@gmail.com>
2024-09-14 17:14:35 -0600

    Delete Workshop1/workshop1.php

--- Workshop1/workshop1.php
+++
@@ -1,8 +0,0 @@
-<?php
-  date_default_timezone_set('America/Costa_Rica');
-
-  $fechaActual = date("d-m-Y");
-  $horaActual = date("h:i:s");
-
-  echo "La fecha es: $fechaActual y la hora es $horaActual " ;
-?>
