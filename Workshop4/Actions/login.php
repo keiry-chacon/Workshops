123e94d8b7ad4fbdd6d918c1bcf57da64ecd147e
keiry-chacon <keirychas@gmail.com>
2024-10-07 22:54:17 -0600

    Add files via upload

---
+++ Workshop4/Actions/login.php
@@ -0,0 +1,17 @@
+<?php
+  require('../utils/functions.php');
+
+  if($_POST) {
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+
+    $user = authenticate($username, $password);
+
+    if($user) {
+      session_start();
+      $_SESSION['user'] = $user;
+      header('Location: ../users.php');
+    } else {
+      header('Location: ../index.php?error=login');
+    }
+  }
